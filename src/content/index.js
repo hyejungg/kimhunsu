c4bc2c9cae32203062d601f64a34addc5867c086
hyejungg <kimhyejung12@naver.com>
2024-11-25 21:04:19 +0900

    코드 정리

--- src/content/index.js
+++ src/content/index.js
@@ -7,7 +7,7 @@ document.addEventListener('click', (event) => {
         tagName: event.target.tagName,
         id: event.target.id,
         classList: Array.from(event.target.classList),
-        innerText: event.target.innerText
+        value: event.target.outerHTML
     };
 
     clearTimeout(debounceTimeout); // 기존 타이머 취소
@@ -15,7 +15,7 @@ document.addEventListener('click', (event) => {
         console.log('@@ Debounced event triggered');
         // 클릭 정보를 백그라운드 스크립트로 전달
         chrome.runtime.sendMessage({
-            type: 'user_click',
+            type: 'click',
             data: elementInfo
         });
     }, 6000); // 6000ms (60s) 지연
@@ -27,24 +27,20 @@ document.addEventListener('keydown', (event) => {
     // 현재 입력 중인 요소를 감지 (input 또는 textarea)
     const activeElement = document.activeElement;
 
-    // activeElement가 입력 가능한 요소인지 확인
     if (activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA') {
-        // Enter 키를 눌렀을 때만 처리
         if (event.key === 'Enter') {
-            const inputValue = activeElement.value; // 현재 입력된 값
+            const elementInfo = {
+                tagName: activeElement.tagName,
+                id: activeElement.id,
+                classList: Array.from(activeElement.classList),
+                value: activeElement.value,
+            };
 
             // 입력된 값을 백그라운드 스크립트로 전달
             chrome.runtime.sendMessage({
-                type: 'user_input',
-                data: {
-                    tagName: activeElement.tagName,
-                    id: activeElement.id,
-                    classList: Array.from(activeElement.classList),
-                    value: inputValue
-                }
+                type: 'search',
+                data: elementInfo
             });
-
-            activeElement.value = '';
         }
     }
 });
